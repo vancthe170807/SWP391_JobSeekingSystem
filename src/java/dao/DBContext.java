f7cdc0ce2b2732263a83040cb3ab18ad559be08c
HaiNam <ninhnamhp@gmail.com>
2024-09-10 20:57:04 +0700

    init project

---
+++ src/java/dao/DBContext.java
@@ -0,0 +1,28 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBContext {
+
+    public Connection connection;
+
+    public DBContext() {
+        try {
+            //Change the username password and url to connect your own database
+            String username = "sa";
+            String password = "123";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Insurance";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new DBContext().connection);
+    }
+}
